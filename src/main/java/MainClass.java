db9e264c300dca2d9bdfc4da8ce3c69996d11ba3
JellyLu <634725546@qq.com>
2016-07-22 00:10:35 +0800

    draw a horizontial line with input numer

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,8 +1,19 @@
+import sun.applet.Main;
+import tools.ConsoleTool;
+import tools.Draw;
+
 /**
  * Created by yjlu@thoughtworks.com on 7/21/16.
  */
 public class MainClass {
+    public static void drawAHorizontialLine() {
+        ConsoleTool.log("please input a number: ");
+        int n = Integer.parseInt(ConsoleTool.inputFromConsole());
+        Draw.drawAHorizontialLine(n);
+    }
+
     public static void main(String args[]) {
-       System.out.println("Hello Java");
+       MainClass.drawAHorizontialLine();
     }
 }
+
--- src/main/java/tools/Draw.java
+++ src/main/java/tools/Draw.java
@@ -4,5 +4,19 @@ package tools;
  * Created by yjlu@thoughtworks.com on 7/21/16.
  */
 public class Draw {
+
+    public static void drawAHorizontialLine(int n) {
+        if (n < 0) {
+            ConsoleTool.printInputInvalidError();
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            ConsoleTool.log("*");
+            if (i == n - 1 ) {
+                ConsoleTool.log("\n");
+            }
+        }
+    }
+
     
 }
