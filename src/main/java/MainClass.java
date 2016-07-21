4ea80a8d3bfb229b838d826d0d0a1ab0fd973997
JellyLu <634725546@qq.com>
2016-07-22 00:16:13 +0800

    Draw a vertical line with input number

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,8 +1,17 @@
+import tools.ConsoleTool;
+import tools.Draw;
+
 /**
  * Created by yjlu@thoughtworks.com on 7/21/16.
  */
 public class MainClass {
+    public static void drawAVerticalLine() {
+        ConsoleTool.log("please input number: ");
+        int n = Integer.parseInt(ConsoleTool.inputFromConsole());
+        Draw.drawAVerticalLine(n);
+    }
+
     public static void main(String args[]) {
-       System.out.println("Hello Java");
+       MainClass.drawAVerticalLine();
     }
 }
--- src/main/java/tools/Draw.java
+++ src/main/java/tools/Draw.java
@@ -4,5 +4,14 @@ package tools;
  * Created by yjlu@thoughtworks.com on 7/21/16.
  */
 public class Draw {
+
+    public static void drawAVerticalLine(int n) {
+        if (n < 0) {
+            ConsoleTool.printInputInvalidError();
+        }
+        for (int i = 0; i < n; i++) {
+            ConsoleTool.logln("*");
+        }
+    }
     
 }
