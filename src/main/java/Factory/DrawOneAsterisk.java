c968e89cca494197d8b8d25fe9a8dc245e19973e
JellyLu <634725546@qq.com>
2016-07-23 14:55:30 +0800

    Add DrawOneAsterisk and tests

--- src/main/java/Factory/DrawOneAsterisk.java
+++ src/main/java/Factory/DrawOneAsterisk.java
@@ -1,7 +1,19 @@
 package Factory;
 
+import java.util.ArrayList;
+
 /**
  * Created by yjlu@thoughtworks.com on 7/23/16.
  */
-public class DrawOneAsterisk {
+public class DrawOneAsterisk implements Drawer {
+    public String[] generateData(int n) {
+        return new String[]{"*"};
+    }
+
+    public String formatDataToString(String[] strings) throws Exception{
+        if (strings.length == 0) {
+            throw new Exception("strings is empty!");
+        }
+        return strings[0];
+    }
 }
--- src/test/java/Factory/DrawOneAsteriskTest.java
+++ src/test/java/Factory/DrawOneAsteriskTest.java
@@ -1,7 +1,34 @@
 package Factory;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 /**
  * Created by yjlu@thoughtworks.com on 7/23/16.
  */
 public class DrawOneAsteriskTest {
+
+    private DrawOneAsterisk drawOneAsterisk;
+
+    @Before
+    public void setUp() {
+       drawOneAsterisk = new DrawOneAsterisk();
+    }
+
+    @Test
+    public void should_return_one_asterisk_when_n_is_1() {
+        String[] s = drawOneAsterisk.generateData(1);
+        assertThat(s.length, is(1));
+        assertThat(s[0], is("*"));
+    }
+
+    @Test
+    public void should_return_one_asterisk_when_n_is_100() {
+        String[] s = drawOneAsterisk.generateData(100);
+        assertThat(s.length, is(1));
+        assertThat(s[0], is("*"));
+    }
 }
